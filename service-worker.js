22b76373b3a2339d264b6cbebad0db7ed85dfdf7
studentservices241445-rgb <studentservices241445@gmail.com>
2026-01-12 15:00:21 -0800

    Add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+// Simple service worker for offline support
+const CACHE_NAME = 'ayed-step-courses-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
